28d113d8abc94cdb0bcf175a3cda42147cd50f4b
Sandesh Soni <hello@sandeshsoni.com>
2019-06-28 18:17:18 +0530

    update Island, create island from coordinates when type given

--- islands_engine/lib/islands_engine/island.ex
+++ islands_engine/lib/islands_engine/island.ex
@@ -5,16 +5,26 @@ defmodule IslandsEngine.Island do
   @nforce_keys [:coordinates, :hit_coordinates]
   defstruct [:coordinates, :hit_coordinates]
 
-  def new do
-    %Island{coordinates: MapSet.new(), hit_coordinates: MapSet.new()}
+  # def new do
+  #   %Island{coordinates: MapSet.new(), hit_coordinates: MapSet.new()}
+  # end
+
+  def new(type, %Coordinate{} = upper_left) do
+    with [_|_] = offsets <- offsets(type),
+         %MapSet{} = coordinates <- add_coordinates(offsets, upper_left)
+      do
+      {:ok, %Island{coordinates: coordinates, hit_coordinates: MapSet.new()}}
+      else
+        error -> error
+    end
   end
 
-  defp offset(:square), do: [{0,0}, {0,1}, {1,0}, {1,1}]
-  defp offset(:atoll), do: [{0,0}, {0,1}, {1,1}, {2,1}, {2,0}]
-  defp offset(:dot), do: [{0,0}]
-  defp offset(:l_shape), do: [{0,0},{1,0},{2,0},{2,1}]
-  defp offset(:s_shape), do: [{0,1},{0,2},{1,0},{1,1}]
-  defp offset(_), do: {:error, :invalid_offset}
+  defp offsets(:square), do: [{0,0}, {0,1}, {1,0}, {1,1}]
+  defp offsets(:atoll), do: [{0,0}, {0,1}, {1,1}, {2,1}, {2,0}]
+  defp offsets(:dot), do: [{0,0}]
+  defp offsets(:l_shape), do: [{0,0},{1,0},{2,0},{2,1}]
+  defp offsets(:s_shape), do: [{0,1},{0,2},{1,0},{1,1}]
+  defp offsets(_), do: {:error, :invalid_offset}
 
   def add_coordinates(offsets, upper_left) do
     Enum.reduce_while(offsets, MapSet.new(), fn offset, accu ->
