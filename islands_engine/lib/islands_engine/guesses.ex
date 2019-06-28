be6f87afbe5010698c20f87b71e749fd3952dddd
Sandesh Soni <hello@sandeshsoni.com>
2019-06-28 23:47:07 +0530

    create add hit, miss functions

--- islands_engine/lib/islands_engine/guesses.ex
+++ islands_engine/lib/islands_engine/guesses.ex
@@ -7,4 +7,13 @@ defmodule IslandsEngine.Guesses do
   def new do
     %Guesses{hits: MapSet.new(), misses: MapSet.new()}
   end
+
+  def add(guesses, :hit, coordinate) do
+    update_in(guesses.hits, &MapSet.put(&1,coordinate))
+  end
+
+  def add(guesses, :miss, coordinate) do
+    update_in(guesses.misses, &MapSet.put(&1,coordinate))
+  end
+
 end
