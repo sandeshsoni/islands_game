a2bc8d3f304a241e5792d7fedcc757af024550d1
Sandesh Soni <hello@sandeshsoni.com>
2019-06-28 15:28:58 +0530

    enforce row col keys

--- islands_engine/lib/islands_engine/coordinate.ex
+++ islands_engine/lib/islands_engine/coordinate.ex
@@ -4,6 +4,7 @@ defmodule IslandsEngine.Coordinate do
   @board_range 1..10
 
 
+  @enforce_keys [:row, :col]
   defstruct [:row, :col]
 
   def new(row, col) when row in @board_range and col in @board_range do
