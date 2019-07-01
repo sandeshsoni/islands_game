74c3a483d56dd5c51339360267846e6963ace522
Sandesh Soni <hello@sandeshsoni.com>
2019-07-01 22:48:34 +0530

    test all islands positioned

--- islands_engine/test/board_test.exs
+++ islands_engine/test/board_test.exs
@@ -25,4 +25,25 @@ defmodule IslandsEngine.BoardTest do
     {:error, _} = Board.position_island(board, :dot, dot_island)
   end
 
+  test "all islands positioned" do
+    {:ok, coordinate1} = Coordinate.new(1,1)
+    {:ok, square_island} = Island.new(:square, coordinate1)
+    {:ok, coordinate2} = Coordinate.new(5,2)
+    {:ok, dot_island} = Island.new(:dot, coordinate2)
+    {:ok, coordinate3} = Coordinate.new(3,3)
+    {:ok, l_island} = Island.new(:square, coordinate3)
+    {:ok, coordinate4} = Coordinate.new(6,2)
+    {:ok, atoll_island} = Island.new(:atoll, coordinate4)
+    {:ok, coordinate5} = Coordinate.new(6,5)
+    {:ok, s_shape_island} = Island.new(:s_shape, coordinate5)
+
+    board = Board.new
+    |> Board.position_island(:square, square_island)
+    |> Board.position_island(:l_shape, l_island)
+    |> Board.position_island(:dot, dot_island)
+    |> Board.position_island(:atoll, atoll_island)
+    |> Board.position_island(:s_shape, s_shape_island)
+    assert Board.all_islands_placed?(board)
+  end
+
 end
