a17b2bb43b6effd0d50968331fb185f02e91295e
Sandesh Soni <hello@sandeshsoni.com>
2019-07-09 17:01:56 +0530

    add basic initialise add_player rule with test

--- islands_engine/lib/islands_engine/rules.ex
+++ islands_engine/lib/islands_engine/rules.ex
@@ -1,4 +1,18 @@
 defmodule IslandsEngine.Rules do
+  alias __MODULE__
 
+  defstruct state: :initialized
+
+  def new do
+    %Rules{}
+  end
+
+  def check(%Rules{state: :initialized} = rules, :add_player) do
+    {:ok, %Rules{rules | state: :players_set}}
+  end
+
+  def check(_state, _action) do
+    :error
+  end
 
 end
--- islands_engine/test/rules_test.exs
+++ islands_engine/test/rules_test.exs
@@ -2,4 +2,12 @@ defmodule IslandsEngine.RulesTest do
   use ExUnit.Case
   alias IslandsEngine.Rules
 
+  test "first step add player" do
+    rules = Rules.new()
+
+    {:ok, new_rules} = Rules.check(rules, :add_player)
+
+    # IO.puts inspect(new_rules)
+    assert new_rules.state == :players_set
+  end
 end
